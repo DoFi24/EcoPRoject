f2651398b1a6364ea017e8e5e3f3b310051e3dad
DoFi24 <daniyarabdulhamidov@gmail.com>
2023-07-26 19:15:07 +0600

    df

--- RegistrationClinik/ViewModels/AllInformationWindowViewModel.cs
+++ RegistrationClinik/ViewModels/AllInformationWindowViewModel.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace RegistrationClinik.ViewModels
@@ -20,6 +21,7 @@ namespace RegistrationClinik.ViewModels
             ChangeKartrijCommand = new LambdaCommand(ChangeKartrijCommandExcecute, CanChangeKartrijCommandExcecuted);
             ChangeClientCommand = new LambdaCommand(ChangeClientCommandExcecute, CanChangeClientCommandExcecuted);
             clientId = _clientId;
+            GetAllFilters();
             GetAllInfo();
         }
 
@@ -27,32 +29,7 @@ namespace RegistrationClinik.ViewModels
         {
 
         }
-        public ICommand ChangeKartrijCommand { get; set; }
-        public ICommand ChangeClientCommand { get; set; }
-        private bool CanChangeKartrijCommandExcecuted(object arg)
-        {
-            return true;
-        }
-
-        private void ChangeKartrijCommandExcecute(object obj)
-        {
-            new ChangeKartrijWindow(this,SelectedKartrij.Id).ShowDialog();
-        }
-        private bool CanChangeClientCommandExcecuted(object arg)
-        {
-            return true;
-        }
-
-        private void ChangeClientCommandExcecute(object obj)
-        {
-            using ApplicationConnect db = new();
-            var result = db.DBTable.FirstOrDefault(s=>s.Id == clientId);
-            result.Name = ClientModel.Name;
-            result.Adres = ClientModel.Name;
-            result.PhoneNumber = ClientModel.Name;
-            result.ModelId = ClientModel.ModelId;
-            db.SaveChanges();
-        }
+        
 
         //тут храниться инфо о картрижах клиента
         private ObservableCollection<KartrijInfoModel> kartrijCollection = new();
@@ -64,7 +41,24 @@ namespace RegistrationClinik.ViewModels
                 Set(ref kartrijCollection, value);
             }
         }
-
+        private ObservableCollection<DBFilter> filterCollection = new();
+        public ObservableCollection<DBFilter> FilterCollection
+        {
+            get => filterCollection;
+            set
+            {
+                Set(ref filterCollection, value);
+            }
+        }
+        private DBFilter clientSelectedFilter = new();
+        public DBFilter ClientSelectedFilter
+        {
+            get => clientSelectedFilter;
+            set
+            {
+                Set(ref clientSelectedFilter, value);
+            }
+        }
         //Тут храниться ифно о клиенте
         private DBTable clientModel = new();
         public DBTable ClientModel
@@ -77,6 +71,7 @@ namespace RegistrationClinik.ViewModels
                 Set(ref clientModel, value);
             }
         }
+        
         private KartrijInfoModel selectedKartrij = new();
         public KartrijInfoModel SelectedKartrij
         {
@@ -97,12 +92,39 @@ namespace RegistrationClinik.ViewModels
                 Set(ref filterName, value);
             }
         }
+        public ICommand ChangeKartrijCommand { get; set; }
+        public ICommand ChangeClientCommand { get; set; }
+        private bool CanChangeKartrijCommandExcecuted(object arg)
+        {
+            return true;
+        }
+        private void ChangeKartrijCommandExcecute(object obj)
+        {
+            new ChangeKartrijWindow(this, SelectedKartrij.Id).ShowDialog();
+        }
+        private bool CanChangeClientCommandExcecuted(object arg)
+        {
+            return true;
+        }
+        private void ChangeClientCommandExcecute(object obj)
+        {
+            using ApplicationConnect db = new();
+            var result = db.DBTable.FirstOrDefault(s => s.Id == clientId);
+            result.Name = ClientModel.Name;
+            result.Adres = ClientModel.Name;
+            result.PhoneNumber = ClientModel.Name;
+            result.ModelId = ClientSelectedFilter.Id;
+            db.SaveChanges();
+            MessageBox.Show("Успешно сохранено, можете закрыть окно!");
+        }
+
         //тут берем данные клиента и его картриджы
         public void GetAllInfo()
         {
             KartrijCollection = new ObservableCollection<KartrijInfoModel>();
             using ApplicationConnect db = new();
             ClientModel = db.DBTable.FirstOrDefault(s => s.Id == clientId);
+            ClientSelectedFilter = FilterCollection.FirstOrDefault(s=>s.Id == ClientModel.ModelId);
             var result = db.DBKartrigList.Where(s => s.TableId == clientId).ToArray();
             for (int i = 0; i < result.Length; i++)
                 KartrijCollection.Add(new KartrijInfoModel
@@ -125,6 +147,11 @@ namespace RegistrationClinik.ViewModels
         {
             using ApplicationConnect db = new();
             return db.DBKartrij.FirstOrDefault(s => s.Id == Id).Name;
+        }
+        private void GetAllFilters()
+        {
+            using ApplicationConnect db = new();
+            FilterCollection = new ObservableCollection<DBFilter>(db.DBFilter);
         }
     }
 }
